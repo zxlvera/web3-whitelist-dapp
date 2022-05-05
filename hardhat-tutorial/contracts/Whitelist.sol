80ec6c68265a5e69a8f4f66f88c2b698835675db
zxlvera <xdoki26@gmail.com>
2022-05-05 08:59:22 +0800

    initial commit

---
+++ hardhat-tutorial/contracts/Whitelist.sol
@@ -0,0 +1,30 @@
+
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract Whitelist {
+
+    // Max number of whitelisted addresses allowed
+    uint8 public maxWhitelistedAddresses;
+
+    // if address is whitelisted, set to true
+    mapping(address => bool) public whitelistedAddresses;
+
+    // numAddressWhiteListed to keep track of how many addresses have been
+    // whitelisted
+    uint8 public numAddressWhiteListed;
+
+    constructor(uint8 _maxWhitelistedAddresses) {
+        maxWhitelistedAddresses = _maxWhitelistedAddresses;
+    }
+
+    function addAddressToWhitelist() public {
+            require(!whitelistedAddresses[msg.sender], "Sender has already been whitelisted");
+            require(numAddressWhiteListed < maxWhitelistedAddresses, "More addresses cant be added, limit reached");
+            whitelistedAddresses[msg.sender] = true;
+            numAddressWhiteListed += 1;
+        }
+
+}
