1a84bd7149aacde723545e1c0a4b70b257acf445
zxlvera <xdoki26@gmail.com>
2022-05-05 09:06:53 +0800

    constants

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -3,7 +3,7 @@ import styles from "../styles/Home.module.css";
 import Web3Modal from "web3modal";
 import { providers, Contract } from "ethers";
 import { useEffect, useRef, useState } from "react";
-import { WHITELIST_CONTRACT_ADDRESSi } from "../constants";
+import { WHITELIST_CONTRACT_ADDRESS } from "../constants";
 import Image from "next/image";
 import Whitelist from "../utils/Whitelist.json";
 
